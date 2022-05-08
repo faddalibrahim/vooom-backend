34a3e1d290900ca10b7d759d9776a0ed1ea9708d
Silas Sangmin <sangminsilas@gmail.com>
2022-05-08 17:14:15 +0000

    Driver

--- api/v1/driver/driver.php
+++ api/v1/driver/driver.php
@@ -4,6 +4,46 @@ require_once(__DIR__."/../../../require/headers.require.php");
 
 require_once(__DIR__."/../../../controller/Driver.controller.php");
 
-echo json_encode(runDriverTest());
+// echo json_encode(runDriverTest());
+
+// GET REQUESTS
+if($_SERVER['REQUEST_METHOD'] === 'GET'){
+
+    (count($_GET) === 0 
+        ? exit(json_encode(getAllDrivers())) : array_key_exists('driver_id',$_GET)) 
+        ? exit(json_encode(getDriver($_GET['driver_id']))) : exit("hello");
+}
+
+// POST REQUESTS
+if($_SERVER['REQUEST_METHOD'] === 'POST'){
+
+    $data = (array) json_decode(file_get_contents("php://input"));
+
+
+    echo addBus($data['bus_no'], $data['start_loc'], $data['destination'], $data['departure_time'], $data['arrival_time'], $data['capacity'], $data['availability']);
+}
+
+
+// DELETE REQUESTS
+
+if($_SERVER['REQUEST_METHOD'] === 'DELETE'){
+    // echo "yess delete request";
+
+    $data = (array) json_decode(file_get_contents("php://input"));
+
+    return deleteBus($data["bus_id"]);
+    // echo  $data['bus_id'];
+}
+
+// PUT REQUESTS
+
+if($_SERVER['REQUEST_METHOD'] === 'PUT'){
+    // exit(json_encode(var_dump($_POST)));
+    $data = (array) json_decode(file_get_contents("php://input"));
+    
+    echo updateBus($data["bus_id"],$data["bus_no"],$data["start_loc"],$data["destination"],$data["departure_time"],$data["arrival_time"],$data["capacity"],$data["availability"]);
+    
+}
+
 
 ?>
--- controller/Driver.controller.php
+++ controller/Driver.controller.php
@@ -8,4 +8,79 @@ function runDriverTest(){
     return $driver->driverTest();
 }
 
+function getAllDrivers(){
+    $driver = new Driver();
+    $stmt = $driver->getAllDrivers();
+
+    if (!$stmt->rowCount()) exit(json_encode(array('ok' => false, 'data' => array())));
+
+    $all_drivers = array('ok' => true, 'drivers' => array());
+
+    while($data = $stmt->fetch()){
+        extract($data);
+
+        $driver = array(
+            'driver_id' => $driver_id,
+            'bus_id' => $bus_id,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'pin' => $pin
+        );
+
+        array_push($all_drivers['drivers'],$driver);
+    }
+
+    exit(json_encode(array('data' => $all_drivers)));
+}
+
+function getDriver($id){
+    $driver = new Driver();
+    $stmt = $driver->getDriver($id);
+
+    if (!$stmt->rowCount()) exit(json_encode(array('ok' => false, 'message' => "no such driver")));
+
+    $driver = array('ok' => true, 'driver' => null);
+
+    while($data = $stmt->fetch()){
+        extract($data);
+
+        $driverr = array(
+            'driver_id' => $driver_id,
+            'bus_id' => $bus_id,
+            'first_name' => $first_name,
+            'last_name' => $last_name,
+            'pin' => $pin
+        );
+
+        $driver['driver'] = $driverr;
+    }
+
+    exit(json_encode(array('data' => $driver)));
+}
+
+
+function addBus($bus_no, $start_loc, $destination, $departure_time, $arrival_time, $capacity, $availability){
+
+    $driver = new Bus();
+    $result = $driver->addBus($bus_no, $start_loc, $destination, $departure_time, $arrival_time, $capacity, $availability);
+
+    return $result;
+
+}
+
+function deleteBus($bus_id){
+    $driver = new Bus();
+    $result=$driver->deleteBus($bus_id);
+    return $result;
+}
+
+function updateBus($bus_id,$bus_no, $start_loc, $destination, $departure_time, $arrival_time, $capacity, $availability){
+    $driver = new Bus();
+    $result = $driver->updateBus($bus_id,$bus_no, $start_loc, $destination, $departure_time, $arrival_time, $capacity, $availability);
+
+    return $result;
+}
+
+
+
 ?>
